8f6ff365e390b2553f5f63a22b0d3738e8f03d45
francois <francoismazerbourg@gmail.com>
2023-04-17 10:07:54 +0200

    server not working

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/user')]
 
@@ -19,6 +22,4 @@ class UserController extends AbstractController
     //         'users' => $userRepository->findAll(),
     //     ]);
     // }
-
-
 }
